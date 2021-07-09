9b27b6c5a4b32758c3dbb6cad2948487a5b52a5e
c-montgomery <craym87@gmail.com>
2021-07-08 20:58:17 -0700

    fixed gaps, spacing and sizing

--- etch.js
+++ etch.js
@@ -4,24 +4,29 @@ let header = document.querySelector('header');
 let button = document.createElement('button');
 let docHeight = document.documentElement.clientHeight;
 let docWidth = document.documentElement.clientWidth;
-container.style.width = '';
+let containerWidth = 900;
+let containerHeight = 900;
+container.style.width = containerWidth + 'px';
+container.style.height = containerHeight + 'px';
 button.textContent = 'Clear and set size'
 header.appendChild(button);
-let dimension = 32;
-container.style.gridTemplateColumns = 'auto '.repeat(dimension);
-container.style.gridTemplateRows = 'auto '.repeat(dimension);
-
+let dimension = 12;
+let boxHeight = `${(containerHeight/dimension).toFixed(0)}px `;
+let boxWidth = `${(containerHeight/dimension).toFixed(0)}px `;
+container.style.gridTemplateColumns = boxHeight.repeat(dimension);
+container.style.gridTemplateRows = boxHeight.repeat(dimension);
+container.style.gap = '2px';
+console.log(`'${((containerHeight/dimension).toFixed(0))}px '.repeat(dimension)`)
 
 
 let dimensionSquared = dimension**2;
 for (let x = 1; x <= dimensionSquared; x++){
     let box = document.createElement('div');
   
-    box.style.height = (docHeight/dimension).toFixed(1) +'px';
-    box.style.width = (docWidth/dimension).toFixed(1) +'px';
-    box.style.borderRadius = '0px'
-    
-    box.style.margin = '1px';
+    box.style.height = boxHeight;
+    box.style.width = boxWidth;
+    box.style.borderRadius = '3px'
+
     box.style.backgroundColor= 'lightgrey';
     // box.style.border= '1px solid red'
     box.className = `e${x}`
