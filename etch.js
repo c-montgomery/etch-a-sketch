09826c43f38f9d778dde92c1ff939364b11db9ea
c-montgomery <craym87@gmail.com>
2021-07-08 17:04:49 -0700

    color-changing grid elements

--- etch.js
+++ etch.js
@@ -1,24 +1,44 @@
-let container = document.querySelector('.container')
+let container = document.querySelector('.container');
 
-let dimension = 16;
-let dimensionCubed = 256;
-for (let x = 1; x <= 256; x++){
+let header = document.querySelector('header');
+let button = document.createElement('button');
+let docHeight = document.documentElement.clientHeight;
+let docWidth = document.documentElement.clientWidth;
+container.style.width = '';
+button.textContent = 'Clear and set size'
+header.appendChild(button);
+let dimension = 32;
+container.style.gridTemplateColumns = 'auto '.repeat(dimension);
+container.style.gridTemplateRows = 'auto '.repeat(dimension);
+
+
+
+let dimensionSquared = dimension**2;
+for (let x = 1; x <= dimensionSquared; x++){
     let box = document.createElement('div');
-    box.textContent = x;
-    box.className = x
+  
+    box.style.height = (docHeight/dimension).toFixed(1) +'px';
+    box.style.width = (docWidth/dimension).toFixed(1) +'px';
+    box.style.borderRadius = '0px'
+    
+    box.style.margin = '1px';
+    box.style.backgroundColor= 'lightgrey';
+    // box.style.border= '1px solid red'
+    box.className = `e${x}`
     box.addEventListener('mouseenter', addColor)
-    // box.addEventListener('mouseleave', removeColor)
+    box.addEventListener('mouseleave', removeColor)
     container.appendChild(box);
 }
-let fuck = document.querySelector('.4');
-fuck.textContent = 'fuck';
+
 function addColor(e){
-    console.log(e);
-let div  = document.querySelector(`div.${e.target.className}`);
-div.textContent ='fuck';
+let div  = document.querySelector(`.${e.target.className}`);
+div.textContent ='';
+div.style.backgroundColor = 'black';
 
 }
 
 function removeColor(e){
-    pass;
+    let div  = document.querySelector(`.${e.target.className}`);
+    div.textContent ='';
+    div.style.backgroundColor = 'grey';
 }
