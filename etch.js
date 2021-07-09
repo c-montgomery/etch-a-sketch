dad1a2b80a10a897aea951a362fd2a48dc5c1d32
c-montgomery <craym87@gmail.com>
2021-07-09 01:19:08 -0700

    added createGrid function on button press

--- etch.js
+++ etch.js
@@ -1,28 +1,30 @@
 let container = document.querySelector('.container');
 
+
 let header = document.querySelector('header');
 let button = document.createElement('button');
 let docHeight = document.documentElement.clientHeight;
 let docWidth = document.documentElement.clientWidth;
-let containerWidth = 900;
-let containerHeight = 900;
+
+let containerWidth = 800;
+let containerHeight = 800;
 container.style.width = containerWidth + 'px';
 container.style.height = containerHeight + 'px';
 button.textContent = 'Clear and set size'
+button.addEventListener('click', createGrid);
 header.appendChild(button);
-let dimension = 12;
+let dimension = 50;
 let boxHeight = `${(containerHeight/dimension).toFixed(0)}px `;
 let boxWidth = `${(containerHeight/dimension).toFixed(0)}px `;
 container.style.gridTemplateColumns = boxHeight.repeat(dimension);
 container.style.gridTemplateRows = boxHeight.repeat(dimension);
 container.style.gap = '2px';
-console.log(`'${((containerHeight/dimension).toFixed(0))}px '.repeat(dimension)`)
 
 
 let dimensionSquared = dimension**2;
 for (let x = 1; x <= dimensionSquared; x++){
     let box = document.createElement('div');
-  
+
     box.style.height = boxHeight;
     box.style.width = boxWidth;
     box.style.borderRadius = '3px'
@@ -33,6 +35,30 @@ for (let x = 1; x <= dimensionSquared; x++){
     box.addEventListener('mouseenter', addColor)
     box.addEventListener('mouseleave', removeColor)
     container.appendChild(box);
+}
+function createGrid(){
+    removeChildren(container);
+    dimension = prompt('what\'s the dimension?');
+    dimensionSquared = dimension**2;
+    let boxHeight = `${(containerHeight/dimension).toFixed(0)}px `;
+    let boxWidth = `${(containerHeight/dimension).toFixed(0)}px `;
+    container.style.gridTemplateColumns = boxHeight.repeat(dimension);
+    container.style.gridTemplateRows = boxHeight.repeat(dimension);
+    container.style.gap = '2px';
+    for (let x = 1; x <= dimensionSquared; x++){
+        let box = document.createElement('div');
+    
+        box.style.height = boxHeight;
+        box.style.width = boxWidth;
+        box.style.borderRadius = '3px'
+    
+        box.style.backgroundColor= 'lightgrey';
+        // box.style.border= '1px solid red'
+        box.className = `e${x}`
+        box.addEventListener('mouseenter', addColor)
+        box.addEventListener('mouseleave', removeColor)
+        container.appendChild(box);
+    }
 }
 
 function addColor(e){
@@ -46,4 +72,16 @@ function removeColor(e){
     let div  = document.querySelector(`.${e.target.className}`);
     div.textContent ='';
     div.style.backgroundColor = 'grey';
+}
+
+function setSize(){
+    dimension = prompt('how many squares per side? (less than 100)')
+    createGrid();
+}
+
+
+function removeChildren(parent){
+    while (parent.lastChild){
+        parent.removeChild(parent.lastChild)
+    }
 }
